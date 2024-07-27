538e9727b588610d5a5c3add0e7b74d6bfb78def
TrixOMG <nikitin.pavel.163@gmail.com>
2024-07-27 16:38:42 +0300

    PHP basic course notes from QSOFT added

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+//   $user = 'Patrick';
+  // echo "123 $user". strlen($user) . PHP_EOL;
+
+// ### добавление элемента в конец массива
+
+// $petTypes[] = 'попугай';
+
+// var_dump($petTypes);
+
+// array_unique|array_map
+// $rooms = array_map(fn ($item) => $item['room'], $catalog['categories']);
+// var_dump($rooms); // [1 => 'Детская', 2 => 'Кухня', 3 => 'Кухня']
+// var_dump(array_unique($rooms)); // [1 => 'Детская', 2 => 'Кухня']
+
+// function sum($a, $b, $c = 0) {
+//   return $a + $b - $c;
+// }
+
+// echo sum(1,2, c:2);
+
+
+?>
